013f3a7f6518e73b2b77dde74526e9f7042e1c85
anaclarinha0 <anaclasouza005@gmail.com>
2024-10-03 14:42:51 -0300

    não terminei

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,7 +1,16 @@
 const Cart = require('../models/cart')
 const User = require('../models/user')
 
-async function create_cart( items ,clientId, res){
+async function create_cart( req, res){
+    const {items, clientId} = req.body
+
+    //array é para indicar que tem que te um vetor
+    //lenght é para indicar o tamanho do vetor, que ele tem que ser maior que 0
+
+    if(!Array.isArray(items)|| !items.length || !clientId){
+        return res.status(400).json({ message: 'não tem items!'})
+        
+    }
     let user = await User.findByPk(clientId)
 
     if(!user){
@@ -22,30 +31,40 @@ async function read_cart(req,res){
     return res.status(200).json({ message: 'item adicionado ', cart: cart})
 }
  
-async function up_id(id,items , res){
-  let cart =  await Cart.findByPk(id)
+async function up_id(req, res){
+    let cart =  await Cart.findByPk(id)
 
-  if(!cart){
-    return res.status(404).json({
-        message: 'Carrinho não encontrado'
+    const id = parseInt(req.params.id)
+    const {items} = req.body
+
+    if(!cart){
+        return res.status(404).json({
+            message: 'Carrinho não encontrado'
+        })
+    }
+    
+    if(items) {
+        //fazer depois
+    }
+
+    return res.status(203).json({
+        message: 'Sucesso',
+        carrinho_encontrado: cart
     })
-  }
-  
-  return res.status(200).json({
-    message: 'Sucesso',
-    carrinho_encontrado: cart
-})
-
- 
 }
 
-async function delete_cart(id){
+async function delete_cart(req, res){
+    const id = parseInt(req.params.id)
     let cart = await Cart.findByPk(id)
+
     if(!cart){
-        return false
+        return res.status(201).json("deletado com sucesso!")
     }
+
     await cart.destroy()
-    return true
+    
+    return res.status(404).json("não encontrado")
+
 }
 
 module.exports = {
--- routes/cart.js
+++ routes/cart.js
@@ -2,39 +2,12 @@ const express = require('express')
 const router = express.Router()
 const ccart  = require('../controllers/cart')
 
-router.post("/create", (req, res) => {
-    const {items, client_id} = req.body
-
-    //array é para indicar que tem que te um vetor
-    //lenght é para indicar o tamanho do vetor, que ele tem que ser maior que 0
-
-    if(!Array.isArray(items)|| !items.length || !client_id){
-        return res.status(400).json({ message: 'não tem items!'})
-        
-    }
-
-    return ccart.create_cart(items, client_id, res)
-} )
+router.post("/create", ccart.create_cart )
  
 router.get("/read", ccart.read_cart )
  
-router.put("/update/:id",(req, res) =>{
-    const id = parseInt(req.params.id)
-    const {items} = req.body
-    
-    return ccart.up_id(id,items, res)
+router.put("/update/:id", ccart.up_id)
  
-} )
- 
- 
-router.delete("/delete/:id" ,(req,res) =>{
-    const id = parseInt(req.params.id)
-    if(ccart.delete_cart(id)){
-        return res.status(201).json("deletado com sucesso!")
-    }
-    else{
-    return res.status(404).json("não encontrado")
-    }
-})
+router.delete("/delete/:id" , ccart.delete_cart)
 
 module.exports = router
