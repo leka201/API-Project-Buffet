5d25f4bf3da48d05c5317f0f3e2d31a00a41770a
anaclarinha0 <anaclasouza005@gmail.com>
2025-02-06 15:33:06 -0300

    cart

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 //npm install --save-dev jest supertest
 
 const request = require('supertest');
-const app = require('./api.js'); // Importa a aplicação
+const app = require('cart/api.js'); // Importa a aplicação
 
 describe('Testando a API', () => {
     it('Deve retornar um JSON com status 200', async () => {
@@ -9,6 +9,32 @@ describe('Testando a API', () => {
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('message');
     });
+
+    it('Deve retornar um JSON com status 200', async () => {
+        const response = await request(api).post('/cart/create').send({
+    
+                "items": [
+                    {
+                        "item_id":1,
+                        "qtd":2
+                    },
+                    {
+                        "item_id":7,
+                        "qtd":100
+                    }
+                ],
+                "clientId": 5
+    
+            })
+            ;
+        });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('cart_created')
+
+
+     
+
+
 });
 
 //npx jest 
