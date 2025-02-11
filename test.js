cb84bf0f37d4dddba90c9c2fa0ea696ccbbe1e99
leka201 <alexiaizabelasantos@hotmail.com>
2025-02-11 16:36:35 -0300

    test realizados-party

--- test.js
+++ test.js
@@ -112,8 +112,8 @@ describe('Testes CRUD para API de Usuários', () => {
         const res = await request(app)
             .get(`/item/show/${itemid}`) 
 
-        expect(res.status).toBe(200)
-        expect(res.body).toHaveProperty;('Achei')
+        expect(res.status).toBe(200);
+        expect(Array.isArray(res.body)).toBe;
     });
 
     it('Deve buscar todos os item', async () => {
@@ -123,7 +123,7 @@ describe('Testes CRUD para API de Usuários', () => {
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty('Sucesso')
     });
-
+       
     it('Deve atualizar um item', async () => {
         const res = await request(app)
             .put(`/item/upd/${itemid}`)
@@ -138,13 +138,26 @@ describe('Testes CRUD para API de Usuários', () => {
     });
     //npx jest 
 
+    it('Deve atualizar um usuário', async () => {
+        const res = await request(app)
+            .put(`/item/upd/${userId}`)
+            .send({
+                nome: 'Teste User Atualizado'
+            });
+
+        expect(res.status).toBe(200);
+        console.log(res)
+        expect(res.body).toHaveProperty();
+    });
+
+    let idpary
     it('deve criar uma festa', async () => {
         const res = await request(app)
             .post('/party/create')
             .send({
                 name: "Festa Infantil",
-                cart_id: 1,
-                user_id: 1,
+                cart_id: cartid,
+                user_id: userId,
                 tipo_pag: "cartao",
                 cpf_cnpj:"999.999.999-99",
                 nome_cartao:"nuu",
@@ -154,37 +167,64 @@ describe('Testes CRUD para API de Usuários', () => {
                 valor: 2000
             });
         expect(res.status).toBe(200);
-        expect(res.body).toHaveProperty('id');
+        expect(res.body).toHaveProperty('message','Sucesso');
+        expect(res.body).toHaveProperty('party');
+        idpary = res.body.party.id
     });
 
     it('pagamento com pix', async ()=> {
         const res = await request(app)
-            .post()
-            .send({})
+            .post('/party/create')
+            .send({
+                name: "Festa Infantil",
+                cart_id: cartid,
+                user_id: userId,
+                tipo_pag: "pix",
+                valor: 2000
+            })
 
         expect(res.status).toBe(200);
-        expect(res.body).toHaveProperty('id');
-        token = res.body.token;
+        expect(res.body).toHaveProperty('message','Sucesso');
+        expect(res.body).toHaveProperty('party');
     });
 
     it('Buscar a Festa criada', async () => {
         const res = await request(app)
-            .get('')
+            .get('/party/read/')
         
         expect(res.status).toBe(200);
-        expect(Array.isArray(res.body)).toBe(true);
+        expect(res.body).toHaveProperty('message','Sucesso');
+        expect(res.body).toHaveProperty('db');
     });
 
     it('Atualizar Festa', async () => {
         const res = await request(app)
-            .put('')
-            .set('')
-            .send({})
+            .put(`/party/update/${idpary}`)
+            .send({
+                name: "Festa Infantil",
+                cart_id: cartid,
+                user_id: userId,
+                pix: "pix",
+                cpf_cnpj:"",
+                nome_do_cartao:"",
+                numero_do_cartao:"",
+                validade:"",
+                CVV:""
+            });
         
         expect(res.status).toBe(200);
-        expect(res.body).toHaveProperty('')
+        expect(res.body).toHaveProperty('message','Festa Atualizada com sucesso');
     });
 
+
+    it('Deletar Festa', async () => {
+        const res = await request(app)
+            .delete(`/party/del/${idpary}`)
+        
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('message','Foi de base');
+    })
+        
 
     it('Deve deletar um Item', async () => {
         const res = await request(app)
